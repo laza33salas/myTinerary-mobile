e01bbafb1d47bddeb1cec1d6c9bf149c240bcabd
laza33salas <lazarocode@gmail.com>
2022-09-28 20:13:04 -0300

    create folder screens and screens cities, details

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>DetailsScreen</Text>
+      <Text>DetailsScreen</Text>
+      <Text>DetailsScreen</Text>
+    </View>
+  )
+}
+
+export default DetailsScreen
