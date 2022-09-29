6a18bcdcb4a0e5fb2dd3bb1c1acd54dbf1c15fb8
LeonardoMedici <leomedici06@gmail.com>
2022-09-29 17:39:12 -0300

    Carousel mobile

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,22 +1,60 @@
 import React from 'react'
-import { ScrollView, ImageBackground, StyleSheet, Button, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { ImageBackground, StyleSheet, View, Text } from 'react-native';
 
 export default function Carousel({ }) {
 
+    const range = 1 
+    const [start, setStart] = useState(0)
+    const [end, setEnd] = useState(start + range)
+    const [intervalId, setIntervalId] = useState()
+
+    const citiesData = [
+        {
+            photo: require('../assets/HongKong.jpeg'),
+            city: 'Hong Kong',
+            alt: 'hongkong'
+        },
+        {
+            photo: require('../assets/Bangkok.jpeg'),
+            city: 'Bangkok',
+            alt: 'bangkokab'
+        },
+        {
+            photo: require('../assets/Bangkok.jpeg'),
+            city: 'BangkokKOKOKOKOK',
+            alt: 'bangkokab'
+        },
+    ]
+
     const cardCarousel = (data) => (
-        <View>
-            <ImageBackground style={styles.carouselImage} source={require('../assets/HongKong.jpeg')}>
-                <Text style={styles.cityNemeIndex}>Hong Kong</Text>
+        <View key={data.alt}>
+            <ImageBackground style={styles.carouselImage} source={data.photo}>
+                <Text style={styles.cityNemeIndex}>{data.city}</Text>
             </ImageBackground>
         </View>
     )
 
+    function next() {
+        if (end < citiesData.length) {
+            setEnd(end + range)
+            setStart(start + range)
+        } else {
+            setStart(0)
+            setEnd(range)
+        }
+    }
+
+    useEffect(() => {
+        let id = setInterval(function () {
+            next()
+        }, 4000)
+        setIntervalId(id)
+        return () => clearInterval(intervalId)
+    }, [start])
+
     return (
-        <View>
-            <ImageBackground style={styles.carouselImage} source={require('../assets/HongKong.jpeg')}>
-                <Text style={styles.cityNemeIndex}>Hong Kong</Text>
-            </ImageBackground>
-        </View>
+        citiesData.slice(start,end).map(cardCarousel)
     )
 }
 
