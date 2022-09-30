83d8d110cf2c28a1ef1eb912395c25a5267f048c
LeonardoMedici <leomedici06@gmail.com>
2022-09-29 21:29:44 -0300

    error

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Index from '../screens/Index'
 import CitiesScreen from '../screens/CitiesScreen'
 import DetailsScreen from '../screens/DetailsScreen'
-<<<<<<< HEAD
 import SignUp from '../screens/SignUp'
 
 const DrawerNavigator = createDrawerNavigator()
@@ -16,20 +15,6 @@ const Drawer = () => {
     <DrawerNavigator.Screen name="Sign Up" component={SignUp} />
   </DrawerNavigator.Navigator>
   )
-=======
-import SignUp from '../screens/SignUp';
-
-const DrawerNavigator = createDrawerNavigator()
-const Drawer = () => {
-    return (
-        <DrawerNavigator.Navigator initialRouteName="Home">
-            <DrawerNavigator.Screen name="SignUp" component={SignUp} />
-            <DrawerNavigator.Screen name="Home" component={Index} />
-            <DrawerNavigator.Screen name="Cities" component={CitiesScreen} />
-            <DrawerNavigator.Screen name="Details" component={DetailsScreen} />
-        </DrawerNavigator.Navigator>
-    )
->>>>>>> 82ce35119307dd18b5aa0695200f6295c5639e77
 }
 
 export default Drawer
