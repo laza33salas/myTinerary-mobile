70e03dc416637678a64b2937aef2ff31e530ced4
laza33salas <lazarocode@gmail.com>
2022-09-30 11:51:20 -0300

    add redux  itineraries

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import citiesApi from './actions/citiesApi'
 import usersApi from './actions/usersApi'
+import itiApi from "./actions/itinerariesApi";
 export default configureStore({
     reducer: {
         cities: citiesApi,
@@ -8,10 +9,13 @@ export default configureStore({
 
         users: usersApi,
         [usersApi.reducerPath]: usersApi.reducer,
+
+        itineraries: itiApi,
+        [itiApi.reducerPath]: itiApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         inmutableCheck: false,
         serializableCheck: false,
-    }).concat(citiesApi.middleware)
+    }).concat(citiesApi.middleware, itiApi.middleware, usersApi.middleware)
 })
