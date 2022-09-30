dd783f0a512c9a31cae5f3f5c38a7f33b88c66ef
laza33salas <lazarocode@gmail.com>
2022-09-30 13:16:48 -0300

    activities store

--- redux/store.js
+++ redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import citiesApi from './actions/citiesApi'
 import usersApi from './actions/usersApi'
 import itiApi from "./actions/itinerariesApi";
+import activitiesApi from './actions/activitiesApi'
 export default configureStore({
     reducer: {
         cities: citiesApi,
@@ -12,10 +13,13 @@ export default configureStore({
 
         itineraries: itiApi,
         [itiApi.reducerPath]: itiApi.reducer,
+
+        activities: activitiesApi,
+        [activitiesApi.reducerPath]: activitiesApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         inmutableCheck: false,
         serializableCheck: false,
-    }).concat(citiesApi.middleware, itiApi.middleware, usersApi.middleware)
+    }).concat(activitiesApi.middleware, citiesApi.middleware, itiApi.middleware, usersApi.middleware)
 })
